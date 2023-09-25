52b542e86a2ea3e02b7ea4b4bf1eb7e0325d2a0e
Deliasafarisa <safarisadelia@gmail.com>
2023-09-25 13:12:22 +0700

    pernerbit.php

---
+++ perpustakaan/app/Controllers/pernerbit.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class penerbit extends BaseController
+{
+    public function penerbit()
+    {
+
+    }
+
+    public function menampilkandaftarpenerbit()
+    {
+
+    }
+}
