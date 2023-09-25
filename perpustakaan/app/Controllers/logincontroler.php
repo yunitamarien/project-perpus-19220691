e40f02c39ff06152c5e542db9b759f6548a43b17
yunitamarien <marienyunita501@gmail.com>
2023-09-25 12:47:13 +0700

    logincontroler.php

---
+++ perpustakaan/app/Controllers/logincontroler.php
@@ -0,0 +1,25 @@
+<?php 
+
+namespace App\Controllers;
+
+use CodeIgniter\controller;
+
+class logincontroller extends BaseController 
+
+{
+    public function login()
+    {
+
+    }
+
+    public function lupapassword()
+    {
+
+    }
+
+    public function daftar() 
+    {
+        
+    }
+}
+
