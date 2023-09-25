817283b1e80e55bde955af3d3e7aacb28aa5767b
Deliasafarisa <safarisadelia@gmail.com>
2023-09-25 12:54:09 +0700

    kategori

---
+++ perpustakaan/app/Controllers/kategori.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class kategori extends BaseController
+{
+    public function pencariankategori()
+    {
+
+    }
+
+    public function menampilkandaftarkategori()
+    {
+
+    }
+}
