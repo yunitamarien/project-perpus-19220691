6663ef031f4643141b4a28638668ee1e5e0f341f
ayuarisandi <ayuarisandi20@gmail.com>
2023-09-25 12:43:07 +0700

    bukucontroller.php

---
+++ perpustakaan/app/Controllers/bukucontroller.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class BukuController extends BaseController
+{
+    public function judul()
+    {
+        
+    }
+
+    public function penulis()
+    {
+    
+    }
+
+    public function sampulbuku()
+    {
+    
+    }
+
+
+}
+
