4032281612093819f384b46cc08cbaeaff7f78d9
ayuarisandi <ayuarisandi20@gmail.com>
2023-09-25 13:00:19 +0700

    berandacontroller.php

---
+++ perpustakaan/app/Controllers/berandacontroller.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class berandacontroller extends BaseController
+{
+    public function halamanutama()
+    {
+        
+    }
+
+    public function pencarian()
+    {
+    
+    }
+
+    public function profile()
+    {
+    
+    }
+
+
+}
+
