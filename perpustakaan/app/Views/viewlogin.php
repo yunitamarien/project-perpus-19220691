5d752cd4877ce2019a7b5ba9d9281f4f3501b25c
yunitamarien <marienyunita501@gmail.com>
2023-10-09 10:08:49 +0700

    view

---
+++ perpustakaan/app/Views/viewlogin.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <h2>Login</h2>
+    <form action="process_login.php" method="post">
+        <label for="daftar">daftar:</label>
+        <input type="text" id="daftar" name="daftar" required><br><br>
+
+        <label for="lupapassword">lupaPassword:</label>
+        <input type="lupapassword" id="lupapassword" name="lupapassword" required><br><br>
+
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html>
